ea394a3cf35394be76fe0d9d9a26978b77baa21d
Zhanming Cui <zhanming.cui@synchronoss.com>
2018-01-09 18:07:38 +0000

    PULL mode

--- foo-service/src/main/java/net/famunity/csihome/spring/cloud/microservices/foo/FooServiceApplication.java
+++ foo-service/src/main/java/net/famunity/csihome/spring/cloud/microservices/foo/FooServiceApplication.java
@@ -1,12 +1,8 @@
 package net.famunity.csihome.spring.cloud.microservices.foo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -14,19 +10,6 @@ public class FooServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FooServiceApplication.class, args);
-	}
-}
-
-@RefreshScope
-@RestController
-class MessageRestController {
-
-	@Value("${foo.service.message:Hello default}")
-	private String message;
-
-	@RequestMapping("/message")
-	String getMessage() {
-		return this.message;
 	}
 }
 
