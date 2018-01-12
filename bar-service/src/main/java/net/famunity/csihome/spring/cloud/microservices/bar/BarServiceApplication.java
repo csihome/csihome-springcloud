47cfe64d6dd71b1fb0b622354fddd58f1b30a126
Zhanming Cui <zhanming.cui@synchronoss.com>
2018-01-12 14:45:06 +0000

    App service discover config-server by service-id

--- bar-service/src/main/java/net/famunity/csihome/spring/cloud/microservices/bar/BarServiceApplication.java
+++ bar-service/src/main/java/net/famunity/csihome/spring/cloud/microservices/bar/BarServiceApplication.java
@@ -2,10 +2,10 @@ package net.famunity.csihome.spring.cloud.microservices.bar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class BarServiceApplication {
 
 	public static void main(String[] args) {
