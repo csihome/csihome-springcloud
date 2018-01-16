763c13e03ded07cf0bdf5e1fda9a7626726cdad5
Zhanming Cui <zhanming.cui@synchronoss.com>
2018-01-16 11:59:58 +0000

    App service discover config-server by service-id

--- foo-service/src/main/java/net/famunity/csihome/spring/cloud/microservices/configuration/ApplicationConfig.java
+++ foo-service/src/main/java/net/famunity/csihome/spring/cloud/microservices/configuration/ApplicationConfig.java
@@ -1,13 +1,10 @@
 package net.famunity.csihome.spring.cloud.microservices.configuration;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-
-@EnableFeignClients
 @Configuration
 public class ApplicationConfig {
 
--- foo-service/src/main/java/net/famunity/csihome/spring/cloud/microservices/controller/MessageRestController.java
+++ foo-service/src/main/java/net/famunity/csihome/spring/cloud/microservices/controller/MessageRestController.java
@@ -5,8 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -20,12 +25,22 @@ class MessageRestController {
 	@Autowired
 	private DiscoveryClient discoveryClient;
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@RequestMapping("/foo")
 	String getMessage() {
-		return this.message +"\n"+ serviceUrl();
+		ResponseEntity responseEntity = restTemplate.exchange(
+				"http://bar-service/bar",
+				HttpMethod.GET,
+				HttpEntity.EMPTY,
+				ParameterizedTypeReference.forType(String.class),
+				"");
+
+		return this.message + "<br/><br/>And also bar-service response from " + serviceUrl() + ": <br/><br/>" + responseEntity.toString();
 	}
 
-	public String serviceUrl() {
+	private String serviceUrl() {
 		List<ServiceInstance> list = discoveryClient.getInstances("bar-service");
 		if (list != null && list.size() > 0 ) {
 			return list.get(0).getUri().toString();
